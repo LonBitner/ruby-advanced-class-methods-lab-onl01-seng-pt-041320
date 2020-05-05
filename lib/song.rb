5ce1f7744c1ed47485e034d9f5afc19943f9c569
Lon Bitner <lonbitner@gmail.com>
2020-05-05 08:35:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,14 @@ class Song
   
   def self.create
     song = self.new
-    song.all.inlcude?(song)
+    self.all << song
     song
   end
   
-  def self.new_by_name()
-end
+  def self.new_by_name
+    song = self.new
+    song.name = title
+    song
+  end
+
 end
